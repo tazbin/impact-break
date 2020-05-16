e4464b8c0649a2c8af0ef6138362168641e83cb3
tazbin <md.tazbinur.rahaman@gmail.com>
2020-05-16 20:06:11 +0600

    fb meta tag resolved

--- inc/header.php
+++ inc/header.php
@@ -8,17 +8,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- SEO Meta Tags -->
-    <meta name="description" content="Free mobile app HTML landing page template to help you build a great online presence for your app which will convert visitors into users">
-    <meta name="author" content="Inovatik">
+    <meta name="description" content="Hult Prize - Impact Break pracatice website for recruitment task">
 
     <!-- OG Meta Tags to improve the way the post looks when you share the page on LinkedIn, Facebook, Google+ -->
-	<meta property="og:site_name" content="" /> <!-- website name -->
+	<meta property="og:site_name" content="Impact Break - Recruitement Task" /> <!-- website name -->
 	<meta property="og:site" content="" /> <!-- website link -->
 	<meta property="og:title" content=""/> <!-- title shown in the actual shared post -->
 	<meta property="og:description" content="" /> <!-- description shown in the actual shared post -->
-	<meta property="og:image" content="" /> <!-- image link, make sure it's jpg -->
+	<meta property="og:image" content="<?php echo BASE_URL; ?>/img/judges/t.webp" /> <!-- image link, make sure it's jpg -->
 	<meta property="og:url" content="" /> <!-- where do you want your post to link to -->
-	<meta property="og:type" content="article" />
+	<meta property="og:type" content="Website" />
 
     <!-- Website Title -->
     <title>Impact Break - Hult Prize</title>
